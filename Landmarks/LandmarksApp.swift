dbee2e7e584069da89d6667b64df13c69bffb2e5
Dimitri Michel <dimitri.michel@hey.com>
2022-05-07 21:25:42 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dimitri PL on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
